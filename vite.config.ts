5de72e85867c1c3eb4d832e36a66260ea7ecf008
JBraddockm <jbraddockm@gmail.com>
2024-06-21 23:02:37 +0100

    Setup testing environment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 /// <reference types="vite/client" />
+/// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	base: './',
 	plugins: [react(), viteTsconfigPaths()],
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: './src/tests/setup-tests.ts',
+	},
 });
